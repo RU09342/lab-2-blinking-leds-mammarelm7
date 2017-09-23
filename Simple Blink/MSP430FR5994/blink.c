7969fd41a0606c41ab770718eae30846977d20bf
mammarelm7 <mammarelm7@students.rowan.edu>
2017-09-23 14:14:50 -0400

    added folders and files

---
+++ Simple Blink/MSP430FR5994/blink.c
@@ -0,0 +1,45 @@
+/*
+Matt Mammarelli
+9/18/17
+ECE 09342-2
+*/
+#include <msp430.h>
+
+void main(void) {
+
+    //WDTCTL is watchdog timer control
+    //WDTPW watchdog timer + password, is an interrupt
+    //WDTHOLD watchdog timer +hold, if 1 watchdog timer is stopped
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;
+
+    // Disable the GPIO power-on default high-impedance mode
+    // to activate previously configured port settings
+    //PM5CTL0 = Power mode 5 control register 0
+    //LOCKLPM5 = Locks I/O pin, bit is reset by a power cycle
+    //~LOCKLPM5=8'b0 and by anding PM5CTL0 it clears the register
+    PM5CTL0 &= ~LOCKLPM5;
+
+    //Enables port 1 as output
+    P1DIR |= 0x01;
+
+    for(;;)//creates an infinite loop so blinks indefinitely
+    {
+        //declared as volatile to indicate i can change at any time
+        // volatile to prevent optimization.
+        //c compiler optimization can automatically remove code that introduces intended delays
+        volatile unsigned int i;
+
+        //P1OUT contains the value to be output
+        // Toggle P1.0 using exclusive-OR
+        // If the least significant bit is 0, the XOR with BIT0 will result in a change to 0
+        // If the least significant bit is 1, the XOR with BIT0 will result in a change to 1
+        P1OUT ^= 0x01;
+
+        //creates the delay between pulses, determines how fast it blinks
+        i = 50000;
+        //will decrement i unless i = 0
+        do i--;
+        while(i != 0);
+    }
+}
---
+++ Simple Blink/MSP430G2553/blink.c
@@ -0,0 +1,36 @@
+/*
+Matt Mammarelli
+9/18/17
+ECE 09342-2
+*/
+
+#include <msp430.h>				
+
+void main(void)
+{
+    //WDTCTL is watchdog timer control
+    //WDTPW watchdog timer + password, is an interrupt
+    //WDTHOLD watchdog timer +hold, if 1 watchdog timer is stopped
+    // Stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;
+
+	//Enables port 1 as output
+	P1DIR |= 0x01;
+
+	//declared as volatile to indicate i can change at any time
+	// volatile to prevent optimization.
+	//c compiler optimization can automatically remove code that introduces intended delays
+	volatile unsigned int i;
+
+	while(1) //infinite while loop
+	{
+	    //P1OUT contains the value to be output
+	    // Toggle P1.0 using exclusive-OR
+	    // If the least significant bit is 0, the XOR with BIT0 will result in a change to 0
+	    // If the least significant bit is 1, the XOR with BIT0 will result in a change to 1
+		P1OUT ^= 0x01;
+
+		//creates the delay between pulses, determines how fast it blinks
+		for(i=50000; i>0; i--);
+	}
+}
